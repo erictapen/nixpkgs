3c156108675c8cdf44fa1d11b74e826659f5e4e9
Justin Humm <justin.humm@posteo.de>
2017-02-27 18:41:03 +0100

    st: added patch for terminal scrollback

--- pkgs/applications/misc/st/default.nix
+++ pkgs/applications/misc/st/default.nix
@@ -1,5 +1,6 @@
-{ stdenv, fetchurl, pkgconfig, writeText, libX11, ncurses, libXext, libXft, fontconfig
-, conf ? null, patches ? []}:
+{ stdenv, fetchurl, fetchpatch, pkgconfig, writeText, libX11, ncurses, libXext, libXft, 
+fontconfig, conf ? null, patches ? []}:
+
 
 with stdenv.lib;
 
@@ -11,7 +12,12 @@ stdenv.mkDerivation rec {
     sha256 = "00309qiw20rc89696pk8bdr7ik4r1aarik7jxqk8k66cdj80v1zp";
   };
 
-  inherit patches;
+  patches = [
+    (fetchpatch {
+      url = "http://st.suckless.org/patches/st-scrollback-0.7.diff";
+      sha256 = "1dng2hfda3hlrfiw0sq00k57yppmqlqk2wa4dd5pmmx8b9db28gp";
+    })
+  ];
 
   configFile = optionalString (conf!=null) (writeText "config.def.h" conf);
   preBuild = optionalString (conf!=null) "cp ${configFile} config.def.h";
