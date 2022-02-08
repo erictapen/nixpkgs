703348eec259879e95f70c03ddf1ce469095d171
Kerstin Humm <kerstin@erictapen.name>
2022-02-08 16:28:03 +0100

    nixos/kanidm: better documentation
    Co-Authored-By:  Martin Weinelt <mweinelt@users.noreply.github.com>

--- nixos/modules/services/security/kanidm.nix
+++ nixos/modules/services/security/kanidm.nix
@@ -51,22 +51,21 @@ let
 in
 {
   options.services.kanidm = {
-    enable = lib.mkEnableOption "the kanidm server";
-    enablePam = lib.mkEnableOption "the pam and nsswitch integration of kanidm";
+    enable = lib.mkEnableOption "the Kanidm server";
+    enablePam = lib.mkEnableOption "the Kanidm client for PAM and NSS integration.";
 
     package = lib.mkOption {
       type = lib.types.package;
       default = pkgs.kanidm;
-      description = "Which kanidm package to use.";
+      description = "Which Kanidm package to use.";
     };
 
     ensureDomainName = lib.mkOption {
       description = ''
-        The domain_name that Kanidm manages. It must be a descendant or equal to the domain
-        specified in settings.origin. It is always set when the server starts up.
-        While in theory it could be changed later, you should carefully consider these
-        warnings:
-        <link xlink:href="https://kanidm.github.io/kanidm/administrivia.html#rename-the-domain"/>
+        The <literal>domain_name</literal> that Kanidm manages. Must be below or equal to the domain
+        specified in <literal>serverSettings.origin</literal>. Will always be set when the server starts up.
+        While it is possible to change this setting later, carefully consider the warnings in the
+        <link xlink:href="https://kanidm.github.io/kanidm/administrivia.html#rename-the-domain">relevant documentation</link>.
       '';
       example = "example.org";
       type = lib.types.str;
@@ -83,18 +82,20 @@ in
             type = lib.types.str;
           };
           ldapbindaddress = lib.mkOption {
-            description = "Address/port combination the emulated ldap server binds to.";
+            description = ''
+              Address and port the LDAP server is bound to. Setting this to an empty string disables the LDAP interface.
+            '';
             example = "[::1]:636";
             default = "";
             type = lib.types.str;
           };
           origin = lib.mkOption {
-            description = "The origin of your kanidm instance. Must have https as protocol.";
+            description = "The origin of your Kanidm instance. Must have https as protocol.";
             example = "https://idm.example.org";
             type = lib.types.strMatching "^https://.*";
           };
           db_path = lib.mkOption {
-            description = "Path to kanidm database.";
+            description = "Path to Kanidm database.";
             default = "/var/lib/kanidm/kanidm.db";
             readOnly = true;
             type = lib.types.path;
@@ -105,7 +106,7 @@ in
             type = lib.types.enum [ "default" "verbose" "perfbasic" "perffull" ];
           };
           role = lib.mkOption {
-            description = "The role of this server. This affects features available and how replication may interact.";
+            description = "The role of this server. This affects the replication relationship and thereby available features.";
             default = "WriteReplica";
             type = lib.types.enum [ "WriteReplica" "WriteReplicaNoUI" "ReadOnlyReplica" ];
           };
@@ -113,10 +114,10 @@ in
       };
       default = { };
       description = ''
-        Settings for kanidm, see
+        Settings for Kanidm, see
         <link xlink:href="https://github.com/kanidm/kanidm/blob/master/kanidm_book/src/server_configuration.md">the documentation</link>
-        and <link xlink:href="https://github.com/kanidm/kanidm/blob/master/examples/server.toml"/>
-        for the example config.
+        and <link xlink:href="https://github.com/kanidm/kanidm/blob/master/examples/server.toml">example configuration</link>
+        for possible values.
       '';
     };
 
@@ -125,13 +126,13 @@ in
         freeformType = settingsFormat.type;
 
         options.uri = lib.mkOption {
-          description = "Address of the kanidm server.";
+          description = "Address of the Kanidm server.";
           example = "http://127.0.0.1:8080";
           type = lib.types.str;
         };
       };
       description = ''
-        Configure kanidm clients, needed for the PAM daemon.
+        Configure Kanidm clients, needed for the PAM daemon.
         See <link xlink:href="https://github.com/kanidm/kanidm/blob/master/kanidm_book/src/client_tools.md#kanidm-configuration">the documentation</link>
         for possible settings.
       '';
@@ -142,13 +143,13 @@ in
         freeformType = settingsFormat.type;
 
         options.pam_allowed_login_groups = lib.mkOption {
-          description = "kanidm group that is allowed to login in PAM";
+          description = "Kanidm groups that are allowed to login using PAM.";
           example = "my_pam_group";
           type = lib.types.listOf lib.types.str;
         };
       };
       description = ''
-        Configure kanidm unix daemon.
+        Configure Kanidm unix daemon.
         See <link xlink:href="https://github.com/kanidm/kanidm/blob/master/kanidm_book/src/pam_and_nsswitch.md#the-unix-daemon">the documentation</link>
         for possible settings.
       '';
@@ -178,7 +179,7 @@ in
           assertion = !cfg.enablePam || options.services.kanidm.clientSettings.isDefined;
           message = ''
             <option>services.kanidm.clientSettings</option> needs to be configured
-            for the PAM daemon to connect to the kanidmd server.
+            for the PAM daemon to connect to the Kanidm server.
           '';
         }
         {
@@ -221,7 +222,7 @@ in
     };
 
     systemd.services.kanidm-unixd = lib.mkIf cfg.enablePam {
-      description = "kanidm PAM daemon";
+      description = "Kanidm PAM daemon";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
       restartTriggers = [ unixConfigFile clientConfigFile ];
@@ -255,7 +256,7 @@ in
     };
 
     systemd.services.kanidm-unixd-tasks = lib.mkIf cfg.enablePam {
-      description = "kanidm PAM home management daemon";
+      description = "Kanidm PAM home management daemon";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" "kanidm-unixd.service" ];
       partOf = [ "kanidm-unixd.service" ];
@@ -317,14 +318,14 @@ in
     users.users = lib.mkMerge [
       (lib.mkIf cfg.enable {
         kanidm = {
-          description = "kanidm server";
+          description = "Kanidm server";
           isSystemUser = true;
           group = "kanidm";
         };
       })
       (lib.mkIf cfg.enablePam {
         kanidm-unixd = {
-          description = "kanidm PAM daemon";
+          description = "Kanidm PAM daemon";
           isSystemUser = true;
           group = "kanidm-unixd";
         };
