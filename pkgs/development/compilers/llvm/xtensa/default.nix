70895e202b5658c0bbe58aacfa92c2e056b0646d
Kerstin Humm <kerstin@erictapen.name>
2022-04-15 21:30:41 +0200

    llvmPackages_xtensa: esp-13.0.0-20211203 -> esp-14.0.0-20220415

--- pkgs/development/compilers/llvm/xtensa/default.nix
+++ pkgs/development/compilers/llvm/xtensa/default.nix
@@ -1,17 +1,17 @@
-{ lib, llvmPackages_13, fetchFromGitHub }:
+{ lib, llvmPackages_14, fetchFromGitHub }:
 let
-  version = "esp-13.0.0-20211203";
+  version = "esp-14.0.0-20220415";
 in
 lib.mapAttrs
   (_: drv: drv.overrideAttrs (old: {
     inherit version;
     meta.maintainers = [ lib.maintainers.erictapen ];
   }))
-  (llvmPackages_13.override {
+  (llvmPackages_14.override {
     llvmSources = fetchFromGitHub {
       owner = "espressif";
       repo = "llvm-project";
       rev = version;
-      sha256 = "sha256-9UZa7HLrwc9eEz0FXOaqbgYWMB7BeWuuEUVtoLUFx3o=";
+      sha256 = "sha256-goXiXDyY2D0WtdwMPV/5Y37MtGUN+yIOGMuS+0TFic8=";
     };
   })
