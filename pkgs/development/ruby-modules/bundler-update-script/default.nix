787e7da703365ee8c09325490a76a53ce6330276
Kerstin Humm <kerstin@erictapen.name>
2022-05-22 12:10:53 +0200

    bundlerUpdateScript: install the script to bin/
    This makes it usable by nix run: `nix run .#myapp.updateScript`

--- pkgs/development/ruby-modules/bundler-update-script/default.nix
+++ pkgs/development/ruby-modules/bundler-update-script/default.nix
@@ -1,25 +1,20 @@
-{ runtimeShell, lib, writeScript, bundix, bundler, bundler-audit, coreutils, git, nix }:
+{ runtimeShell, lib, writeShellScriptBin, bundix, bundler, bundler-audit, coreutils, git, nix }:
 
 attrPath:
 
-let
-  updateScript = writeScript "bundler-update-script" ''
-    #!${runtimeShell}
-    PATH=${lib.makeBinPath [ bundler bundler-audit bundix coreutils git nix ]}
-    set -o errexit
-    set -o nounset
-    set -o pipefail
+writeShellScriptBin "bundler-update-script" ''
+  PATH=${lib.makeBinPath [ bundler bundler-audit bundix coreutils git nix ]}
+  set -o errexit
+  set -o nounset
+  set -o pipefail
 
-    attrPath=$1
+  toplevel=$(git rev-parse --show-toplevel)
+  position=$(nix eval -f "$toplevel" --raw "${attrPath}.meta.position")
+  gemdir=$(dirname "$position")
 
-    toplevel=$(git rev-parse --show-toplevel)
-    position=$(nix eval -f "$toplevel" --raw "$attrPath.meta.position")
-    gemdir=$(dirname "$position")
+  cd "$gemdir"
 
-    cd "$gemdir"
-
-    bundler lock --update
-    bundler-audit check --update
-    bundix
-  '';
-in [ updateScript attrPath ]
+  bundler lock --update
+  bundler-audit check --update
+  bundix
+''
