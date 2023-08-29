5401a15a7e81b3b2992fabe9ac52634745d973a0
Kerstin Humm <kerstin@erictapen.name>
2023-08-29 15:59:10 +0200

    mobilizon: revert change to mkYarnPackage, solve it by setting yarnPostBuild

--- pkgs/servers/mobilizon/js.nix
+++ pkgs/servers/mobilizon/js.nix
@@ -6,6 +6,13 @@ mkYarnPackage rec {
     src = "${mobilizon-src}/js";
   };
 
+  # Somehow $out/deps/mobilizon/node_modules ends up only containing a .bin
+  # directory otherwise.
+  yarnPostBuild = ''
+    rm -rf $out/deps/mobilizon/node_modules
+    ln -s $out/node_modules $out/deps/mobilizon/node_modules
+  '';
+
   buildPhase = ''
     runHook preBuild
 
