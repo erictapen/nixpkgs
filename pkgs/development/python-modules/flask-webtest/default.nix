61676f0c406ba844793c6e1647e93bfd3dc4cbb4
Kerstin Humm <kerstin@erictapen.name>
2024-08-30 12:50:01 +0200

    python3Packages.flask-webtest: fixup

--- pkgs/development/python-modules/flask-webtest/default.nix
+++ pkgs/development/python-modules/flask-webtest/default.nix
@@ -6,8 +6,6 @@
   flask,
   webtest,
   blinker,
-  flake8,
-  twine,
   flask-sqlalchemy,
   greenlet,
 }:
@@ -21,7 +19,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "level12";
     repo = "flask-webtest";
-    rev = version;
+    rev = "refs/tags/${version}";
     hash = "sha256-4USNT6HYh49v+euCePYkL1gR6Ul8C0+/xanuYGxKpfM=";
   };
 
@@ -34,17 +32,9 @@ buildPythonPackage rec {
   ];
 
   nativeCheckInputs = [
-    flake8
-    twine
     flask-sqlalchemy
     greenlet
   ];
-
-  checkPhase = ''
-    python3 setup.py sdist
-    twine check dist/*
-    flake8 flask_webtest.py tests
-  '';
 
   pythonImportsCheck = [ "flask_webtest" ];
 
