f8b1db88fc59e925f9ef55a2d7353fa921fdd014
Kerstin Humm <kerstin@erictapen.name>
2024-12-07 19:19:22 +0100

    accesstomemory: build frontend

--- pkgs/by-name/ac/accesstomemory/package.nix
+++ pkgs/by-name/ac/accesstomemory/package.nix
@@ -1,21 +1,46 @@
 {
   lib,
   fetchFromGitHub,
+  buildNpmPackage,
+  fetchNpmDeps,
+  lessc,
   php,
   phpCfg ? null,
   phpunit
 }:
 
-php.buildComposerProject (finalAttrs: {
-  pname = "atom";
+let
   version = "2.8.2";
-
   src = fetchFromGitHub {
     owner = "artefactual";
     repo = "atom";
     rev = "dev/php-80-update";
     hash = "sha256-HuMWI+UT/FA+Mr52jqvTU9OphaMpmxwUCsWNEyf+P+0=";
   };
+  frontend = buildNpmPackage rec {
+    pname = "accesstomemory-frontend";
+    inherit version src;
+
+    npmDepsHash = "sha256-w8526//Lm5RhlnKC7T4Wid2ymXViW1aqNSoI1N/0rb0=";
+
+    env.CYPRESS_INSTALL_BINARY = "0"; # disallow cypress from downloading binaries in sandbox
+
+    # Compiling these plugins fails currently
+    # nativeBuildInputs = [ lessc ];
+    # postBuild = ''
+    #   make -C plugins/arDominionPlugin
+    #   make -C plugins/arArchivesCanadaPlugin
+    # '';
+
+    installPhase = ''
+      mkdir $out
+      cp -r dist/* $out/
+    '';
+  };
+in
+php.buildComposerProject (finalAttrs: {
+  pname = "accesstomemory";
+  inherit version src;
 
   composerNoDev = true;
 
@@ -33,5 +58,13 @@ php.buildComposerProject (finalAttrs: {
   vendorHash = "sha256-v8FPCgLWljEFuk/ghyZhye96IGdmlIrYXwvmeu8XbGI=";
 
   # Run unit tests with `composer test`
+
+  postInstall = ''
+    ln -s ${frontend} $out/dist
+  '';
+
+  passthru = {
+    inherit frontend;
+  };
 
 })
