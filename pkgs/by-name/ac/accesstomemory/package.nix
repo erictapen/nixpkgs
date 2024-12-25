3e37689d6317e124ec831d1d41cba4233f2f0584
Kerstin Humm <kerstin@erictapen.name>
2024-12-25 03:50:05 +0100

    accesstomemory: add meta

--- pkgs/by-name/ac/accesstomemory/package.nix
+++ pkgs/by-name/ac/accesstomemory/package.nix
@@ -1,52 +1,15 @@
 {
   lib,
-  applyPatches,
   fetchFromGitHub,
   buildNpmPackage,
   fetchNpmDeps,
   lessc,
   php82,
-  phpCfg ? null,
-  phpunit,
   nixosTests,
 }:
 
 let
-  php = php82;
-  version = "2.8.2";
-  src = fetchFromGitHub {
-    owner = "artefactual";
-    repo = "atom";
-    rev = "dev/php-80-update";
-    hash = "sha256-snU/yZ/5BN1lkDfAcwvl5F3wKRqEqbylcXemGbwi6P8=";
-  };
-  frontend = buildNpmPackage rec {
-    pname = "accesstomemory-frontend";
-    inherit version src;
-
-    npmDepsHash = "sha256-w8526//Lm5RhlnKC7T4Wid2ymXViW1aqNSoI1N/0rb0=";
-
-    env.CYPRESS_INSTALL_BINARY = "0"; # disallow cypress from downloading binaries in sandbox
-
-    nativeBuildInputs = [ lessc ];
-    postBuild = ''
-      make -C plugins/arDominionPlugin
-      make -C plugins/arArchivesCanadaPlugin
-    '';
-
-    installPhase = ''
-      mkdir $out
-      cp -r css dist js images plugins vendor $out/
-    '';
-  };
-in
-php.buildComposerProject (finalAttrs: {
-  pname = "accesstomemory";
-  inherit version src;
-
-  composerNoDev = true;
-
-  php = php.buildEnv (
+  php = php82.buildEnv (
     {
       extensions = (
         { all, enabled }:
@@ -64,14 +27,52 @@ php.buildComposerProject (finalAttrs: {
         ])
       );
     }
-    // lib.optionalAttrs (phpCfg != null) {
-      extraConfig = phpCfg;
-    }
   );
+  version = "2.8.2";
+  src = fetchFromGitHub {
+    owner = "artefactual";
+    repo = "atom";
+    rev = "dev/php-80-update";
+    hash = "sha256-snU/yZ/5BN1lkDfAcwvl5F3wKRqEqbylcXemGbwi6P8=";
+  };
+  meta = with lib; {
+    description = "Open-source, web application for archival description and public access";
+    homepage = "https://accesstomemory.org/";
+    changelog = "https://github.com/artefactual/atom/releases/tag/v${version}";
+    license = with licenses; [ agpl3Plus ];
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ erictapen ];
+  };
+  frontend = buildNpmPackage rec {
+    pname = "accesstomemory-frontend";
+    inherit version src meta;
+
+    npmDepsHash = "sha256-w8526//Lm5RhlnKC7T4Wid2ymXViW1aqNSoI1N/0rb0=";
+
+    env.CYPRESS_INSTALL_BINARY = "0"; # disallow cypress from downloading binaries in sandbox
+
+    nativeBuildInputs = [ lessc ];
+
+    postBuild = ''
+      make -C plugins/arDominionPlugin
+      make -C plugins/arArchivesCanadaPlugin
+    '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r css dist js images plugins vendor $out/
+    '';
+  };
+in
+php.buildComposerProject (finalAttrs: {
+  pname = "accesstomemory";
+  inherit version src meta;
+
+  composerNoDev = true;
+
+  inherit php;
 
   vendorHash = "sha256-B7mccuIPSLjxKMwHn93V1WtQmpFNpkVVxRvblhXWMFE=";
-
-  # Run unit tests with `composer test`
 
   postInstall = ''
     cp -r ${frontend}/* $out/share/php/accesstomemory/
@@ -79,7 +80,7 @@ php.buildComposerProject (finalAttrs: {
 
   passthru = {
     inherit frontend;
-    phpPackage = finalAttrs.php;
+    phpPackage = php;
     tests = { inherit (nixosTests) accesstomemory; };
   };
 
